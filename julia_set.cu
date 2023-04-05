fda0cfef55fcd7c691f595f289fe596928235d27
Pupking <dsh17082001@gmail.com>
2023-04-05 23:10:50 +0530

    GPU use in graphics

--- julia_set.cu
+++ julia_set.cu
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include "reqs/common/book.h"
 #include "reqs/common/cpu_bitmap.h"
+#include <sys/time.h>
+
 
 #ifndef DIM
-#define DIM 1000
+#define DIM 1024
 #endif
 
 struct cuComplex {
@@ -45,29 +47,58 @@ __device__ int julia(int x, int y)
 }
 
 
-__global__ void kernel(unsigned char *ptr)
+__global__ void kernel(unsigned char *ptr, int stride)
 {
     int x = blockIdx.x;
     int y = blockIdx.y;
-    int offset = x + y*gridDim.x;
 
-    int juliaValue = julia(x, y);
-    ptr[offset*4 + 0] = 255*juliaValue;
-    ptr[offset*4 + 1] = 0;
-    ptr[offset*4 + 2] = 0;
-    ptr[offset*4 + 3] = 255;
+    int sIndex_x = x*stride;
+    int fIndex_x = sIndex_x + stride;
+    if(fIndex_x > DIM) fIndex_x = DIM;
+
+    int sIndex_y = y*stride;
+    int fIndex_y = sIndex_y + stride;
+    if(fIndex_y > DIM) fIndex_y = DIM;
+
+    for(int i = sIndex_x; i < fIndex_x; i++)
+    {
+        for(int j = sIndex_y; j < fIndex_y; j++)
+        {
+            int offset = i + j*DIM;
+//            printf("%d %d\n", i, j);
+
+            int juliaValue = julia(i, j);
+            ptr[offset*4 + 0] = 255*juliaValue;
+            ptr[offset*4 + 1] = 0;
+            ptr[offset*4 + 2] = 0;
+            ptr[offset*4 + 3] = 255;
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
+    struct timeval tv1, tv2;
+	struct timezone tz;
+    double elapsedTime;
+
     CPUBitmap bitmap(DIM, DIM);
     unsigned char *dev_bitmap;
     cudaMalloc((void**)&dev_bitmap, bitmap.image_size());
-    dim3 grid(DIM ,DIM);
-    kernel<<<grid, 1>>>(dev_bitmap);
+    int blocks = atoi(argv[1]);
+    
+    int stride = (blocks + DIM - 1)/blocks;
+    printf("S: %d\n", stride);
+
+    dim3 grid(blocks ,blocks);
+    gettimeofday(&tv1,&tz);
+    kernel<<<grid, 1>>>(dev_bitmap, stride);
 
     cudaMemcpy(bitmap.get_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost);
     cudaDeviceSynchronize();
+    gettimeofday(&tv2,&tz);
+    elapsedTime = (double)(tv2.tv_sec - tv1.tv_sec)+(double) (tv2.tv_usec - tv1.tv_usec)*1.0e-6;
+    printf("\n Device(GPU) Execution Time: %lf\n",elapsedTime);
     bitmap.display_and_exit();
     cudaFree(dev_bitmap);
 }
